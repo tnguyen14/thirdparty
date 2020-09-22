8b5eb70f9a7624efd157f2168033d758ed4e0860
Tri Nguyen <tri@tridnguyen.com>
2020-09-21 20:30:31 -0400

    Extract services out to their own files

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 require("dotenv").config();
-const got = require("got");
-const mem = require("mem");
 
 const fastify = require("fastify")({
   ignoreTrailingSlash: true,
 });
 
-const json = got.extend({
-  responseType: "json",
-  resolveBodyOnly: true,
-});
+const omdb = require("./lib/omdb");
+const embedly = require("./lib/embedly");
 
 fastify.register(require("fastify-sensible"));
 
@@ -36,43 +32,15 @@ fastify.get("/", async (request, reply) => {
   reply.send("OK");
 });
 
-async function getOMDB(type, title) {
-  const response = await json.get(
-    `https://omdbapi.com/?apikey=${process.env.OMDB_API_KEY}&type=${type}&t=${title}`
-  );
-  return response;
-}
-
-const cachedGetOMDB = mem(getOMDB, {
-  cacheKey: (arguments_) => arguments_.join(","),
-  maxAge: 86400000, // 24 hours in milliseconds
-});
-
 fastify.get("/omdb", async (request, reply) => {
   handleRequest(request, reply, async ({ type, title }) => {
-    return await cachedGetOMDB(type, title);
+    return await omdb(type, title);
   });
-});
-
-async function extractEmbedly(url) {
-  const response = await json.get(
-    `https://api.embed.ly/1/extract?key=${
-      process.env.EMBEDLY_API_KEY
-    }&url=${encodeURIComponent(url)}&format=json`
-  );
-  if (response.type == "error") {
-    throw new Error(`${response.error_code} - ${response.error_message}`);
-  }
-  return response;
-}
-
-const cachedExtractEmbedly = mem(extractEmbedly, {
-  maxAge: 300000, // cache for 5 minutes
 });
 
 fastify.get("/embedly", async (request, reply) => {
   handleRequest(request, reply, async ({ url }) => {
-    return await cachedExtractEmbedly(url);
+    return await embedly(url);
   });
 });
 
